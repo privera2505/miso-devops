9fe58205b33aae3650d90c959ceef7dfa186d684
privera <p.riverah@uniandes.edu.co>
2025-11-11 20:10:41 -0500

    Se deja conexión con codeconnections

--- terraform/stacks/FargateApp/main.tf
+++ terraform/stacks/FargateApp/main.tf
@@ -7,17 +7,17 @@ module "aws_ecr_repository" {
     ecr_repository_name = var.ecr_repository_name
 }
 
-############################################################
-## RDS Module
-############################################################
-#
-#module "rds" {
-#    source = "../../modules/rds"
-#    identifier = var.identifier
-#    username = var.username
-#    password = var.password
-#    db_name = var.db_name
-#}
+###########################################################
+# RDS Module
+###########################################################
+
+module "rds" {
+    source = "../../modules/rds"
+    identifier = var.identifier
+    username = var.username
+    password = var.password
+    db_name = var.db_name
+}
 
 ###########################################################
 # VPC Module
@@ -94,74 +94,77 @@ module "ecs_cluster" {
     ecs_service_role = module.iam_ecs_servicerole.ecsservicerole_arn
 }
 
-############################################################
-## S3 para versiones Module
-############################################################
-#
-#module "s3_bucket_for_versions" {
-#    source = "../../modules/s3"
-#    environment = var.environment
-#    bucket_name = var.s3_bucket_for_versions
-#}
-#
-############################################################
+###########################################################
+# S3 para versiones Module
+###########################################################
+
+module "s3_bucket_for_versions" {
+    source = "../../modules/s3"
+    environment = var.environment
+    bucket_name = var.s3_bucket_for_versions
+}
+
+#############################################################
 ## CodeConnections for github
 ############################################################
 #
 #module "connection_github" {
 #    source = "../../modules/code_connections_github"
 #}
-#
-############################################################
-## IAM CodeBuild
-############################################################
-#
-#module "iam_codebuild" {
-#    source = "../../modules/iam_codebuild"
-#}
-#
-############################################################
-## Codebuild project configuration
-############################################################
-#
-#module "codebuild_aws" {
-#    source = "../../modules/codebuild"
-#    build_name = var.build_name
-#    iam_codebuild_arn = module.iam_codebuild.codebuild_role_arn
-#    ecr_repo_uri = module.aws_ecr_repository.ecr_repo_uri
-#    source_location = var.source_location
-#    source_type = var.source_type
-#    source_version = var.source_version
-#    codeconnections_arn = module.connection_github.codeconnettions_arn
-#    compute_type = var.compute_type
-#    aws_region = var.aws_region
-#    bucket_versiones = module.s3_bucket_for_versions.bucket_name
-#    db_name = var.db_name
-#    db_host = module.rds.db_host
-#    username = var.username
-#    password = var.password
-#}
-#
-############################################################
-## IAM CodePipeline Module
-############################################################
-#
-#module "iam_codepipeline" {
-#    source = "../../modules/iam_codepipeline"
-#    codeconnections_arn = module.connection_github.codeconnettions_arn
-#}
-#
-############################################################
-## CodePipeline Module
-############################################################
-#
-#module "codepipeline_aws" {
-#    source = "../../modules/codepipeline"
-#    pipeline_name = var.pipeline_name
-#    bucket_versiones = module.s3_bucket_for_versions.bucket_name
-#    source_location = var.source_location
-#    codebuild_name = module.codebuild_aws.project_name
-#    codeconnections_arn = module.connection_github.codeconnettions_arn
-#    source_version = var.source_version
-#    role_arn = module.iam_codepipeline.codepipeline_role_arn
-#}
+
+###########################################################
+# IAM CodeBuild
+###########################################################
+
+module "iam_codebuild" {
+    source = "../../modules/iam_codebuild"
+}
+
+###########################################################
+# Codebuild project configuration
+###########################################################
+
+module "codebuild_aws" {
+    source = "../../modules/codebuild"
+    build_name = var.build_name
+    iam_codebuild_arn = module.iam_codebuild.codebuild_role_arn
+    ecr_repo_uri = module.aws_ecr_repository.ecr_repo_uri
+    source_location = var.source_location
+    source_type = var.source_type
+    source_version = var.source_version
+    codeconnections_arn = "arn:aws:codeconnections:us-east-1:291935881445:connection/f71457b1-01b1-46a3-865e-1e097cb90602"
+    #module.connection_github.codeconnettions_arn
+    compute_type = var.compute_type
+    aws_region = var.aws_region
+    bucket_versiones = module.s3_bucket_for_versions.bucket_name
+    db_name = var.db_name
+    db_host = module.rds.db_host
+    username = var.username
+    password = var.password
+}
+
+###########################################################
+# IAM CodePipeline Module
+###########################################################
+
+module "iam_codepipeline" {
+    source = "../../modules/iam_codepipeline"
+    codeconnections_arn = "arn:aws:codeconnections:us-east-1:291935881445:connection/f71457b1-01b1-46a3-865e-1e097cb90602"
+    #module.connection_github.codeconnettions_arn
+}
+
+###########################################################
+# CodePipeline Module
+###########################################################
+
+module "codepipeline_aws" {
+    source = "../../modules/codepipeline"
+    pipeline_name = var.pipeline_name
+    bucket_versiones = module.s3_bucket_for_versions.bucket_name
+    source_location = var.source_location
+    codebuild_name = module.codebuild_aws.project_name
+    codeconnections_arn = "arn:aws:codeconnections:us-east-1:291935881445:connection/f71457b1-01b1-46a3-865e-1e097cb90602"
+    #module.connection_github.codeconnettions_arn
+    source_version = var.source_version
+    role_arn = module.iam_codepipeline.codepipeline_role_arn
+}
